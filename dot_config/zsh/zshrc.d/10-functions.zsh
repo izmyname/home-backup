c3fd205b5b59794a7a008470dce985c34e4d026e
Izmyname <VI-MCMXCV@proton.me>
2024-12-07 00:49:52 +0500

    upload

---
+++ dot_config/zsh/zshrc.d/10-functions.zsh
@@ -0,0 +1,58 @@
+# called before prompt(?)
+function precmd {
+    # Set window title
+    print -Pn "\e]0;%(1j,%j job%(2j|s|); ,)%~\e\\"
+}
+
+# called when executing a command
+function preexec {
+    print -Pn "\e]0;${(q)1}\e\\"
+}
+
+# foot - spawn new terminal in the current working directory
+function osc7-pwd() {
+    emulate -L zsh # also sets localoptions for us
+    local LC_ALL=C
+    printf '\e]7;file://%s%s\e\' $HOST ${PWD//(#m)([^@-Za-z&-;_~])/%${(l:2::0:)$(([##16]#MATCH))}}
+}
+
+function chpwd-osc7-pwd() {
+    (( ZSH_SUBSHELL )) || osc7-pwd
+}
+add-zsh-hook -Uz chpwd chpwd-osc7-pwd
+
+# file manager keybinds
+cdUndoKey() {
+  popd
+  zle       reset-prompt
+  print
+  ls
+  zle       reset-prompt
+}
+
+cdParentKey() {
+  pushd ..
+  zle      reset-prompt
+  print
+  ls
+  zle       reset-prompt
+}
+
+zle -N                 cdParentKey
+zle -N                 cdUndoKey
+bindkey '^[[1;3A'      cdParentKey
+bindkey '^[[1;3D'      cdUndoKey
+
+# exit on partial command line
+exit_zsh() { exit }
+zle -N exit_zsh
+bindkey '^D' exit_zsh
+
+# yazi - cd on quit
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
